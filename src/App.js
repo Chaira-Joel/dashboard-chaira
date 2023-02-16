20099c3f1dcfbca47c56a86bbf488e2bbea4cc41
Chaïra Joel <chaira.cynthia.joel@gmail.com>
2023-02-16 08:08:11 +0100

    parsed the CSV data to an array

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Checkboxes from "./Components/Checboxes";
 import Barchart from "./Components/Barchart";
 import Table from "./Components/Table";
 import StudentData from "./StudentData/StudentData";
+import HomePage from './Components/Home'
 
 function App() {
   return (
     <div className="App">
 
       <Header/>
+        <HomePage/>
         <Checkboxes/>
         <Barchart/>
         <Table/>
