0c0dd2b3b85f40975193e4c8f05b4a390972888f
Chaïra Joel <chaira.cynthia.joel@gmail.com>
2023-02-19 11:20:23 +0100

    Victory chart now takes assignment and difficulty

--- src/Components/Barchart.js
+++ src/Components/Barchart.js
@@ -3,10 +3,11 @@ import {VictoryBar, VictoryChart, VictoryAxis, VictoryTheme} from "victory";
 
 
 const data = [
-    {quarter: 1, earnings: 13000},
-    {quarter: 2, earnings: 16500},
-    {quarter: 3, earnings: 14250},
-    {quarter: 4, earnings: 19000}
+    {id: 1, name:"piet", assignment: "a45", difficulty:1, fun: 5},
+    {id: 2, name:"klaas", assignment: "8hb", difficulty:5, fun: 4},
+    {id: 3, name:"petra", assignment: "2b", difficulty:4, fun: 2},
+    {id: 4, name:"harrie", assignment: "5b", difficulty:1, fun: 1},
+    {id: 5, name:"petra", assignment: "a45", difficulty:3, fun: 3},
 ];
 
 function Barchart(){
@@ -16,18 +17,18 @@ function Barchart(){
 domainPadding={20}
 theme={VictoryTheme.material}>
     <VictoryAxis
-        tickValues={[1,2,3,4]}
-        tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+        tickValues={[1,2,3,4,5]}
+        tickFormat={data.assignment}
         />
 
     <VictoryAxis
         dependentAxis
-        tickFormat={(x)=>(`$${x /1000}k`)}
+        tickFormat={(x)=>(`${x /1}`)}
         />
             <VictoryBar
             data={data}
-            x={"quarter"}
-            y={"earnings"}/>
+            x={"assignment"}
+            y={"difficulty"}/>
 </VictoryChart>
 
     )
