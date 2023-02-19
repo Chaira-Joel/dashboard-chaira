fc5d7879a8786f4f5afa058ed7d77d9c18f935c1
Chaïra Joel <chaira.cynthia.joel@gmail.com>
2023-02-19 12:13:40 +0100

    created unit test for barchart

--- src/Components/Barchart.js
+++ src/Components/Barchart.js
@@ -12,10 +12,13 @@ const data = [
 
 function Barchart(){
     return(
+        <div data-testid="barchart">
 
-<VictoryChart width={400} height={300}
-domainPadding={20}
-theme={VictoryTheme.material}>
+<VictoryChart
+    width={400}
+    height={300}
+    domainPadding={20}
+    theme={VictoryTheme.material}>
 
     <VictoryAxis
         tickValues={[1,2,3,4,5]}
@@ -27,27 +30,28 @@ theme={VictoryTheme.material}>
         tickFormat={(x)=>(`${x /1}`)}
         />
 
-    <VictoryGroup offset={20} colorScale={"qualitative"}>
+    <VictoryGroup
+        offset={20}
+        colorScale={"qualitative"}>
+
             <VictoryBar
                 barWidth={10}
                 style={{ data: { fill: "red" } }}
-            data={data}
-            x={"assignment"}
-            y={"difficulty"}
+                data={data}
+                x={"assignment"}
+                y={"difficulty"}
        />
 
-    <VictoryBar
-        barWidth={10}
-        style={{ data: { fill: "blue" } }}
-        data={data}
-        x={"assignment"}
-        y={"fun"}
+            <VictoryBar
+             barWidth={10}
+             style={{ data: { fill: "blue" } }}
+               data={data}
+             x={"assignment"}
+             y={"fun"}
     />
     </VictoryGroup>
-
-
 </VictoryChart>
-
+        </div>
     )
 }
 export default Barchart
---
+++ src/UnitTests/Barchart.test.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {render} from "@testing-library/react"
+import Barchart from "../Components/Barchart";
+
+test('Renders a Victory BarChart Component', ()=>{
+    const {getByTestId} = render(<Barchart/>);
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    const barchartComponent = getByTestId("barchart");
+    expect(barchartComponent).toBeInTheDocument();
+
+});
