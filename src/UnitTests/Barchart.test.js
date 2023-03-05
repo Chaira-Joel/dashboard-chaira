e8dee633649ec4be094956a33a8403171c3edee9
Chaira Joel <chaira.cynthia.joel@gmail.com>
2023-03-05 17:53:14 +0100

    Created a unit test for the BarChart component props

--- src/UnitTests/Barchart.test.js
+++ src/UnitTests/Barchart.test.js
@@ -2,11 +2,26 @@ import React from "react"
 import {render} from "@testing-library/react"
 import Barchart from "../Components/Barchart";
 
-test('Renders a Victory BarChart Component', ()=>{
-    const {getByTestId} = render(<Barchart/>);
-    // eslint-disable-next-line testing-library/prefer-screen-queries
-    const barchartComponent = getByTestId("barchart");
-    expect(barchartComponent).toBeInTheDocument();
+describe('Barchart', () => {
 
+    it('Renders a Victory BarChart Component', ()=>{
+        const {getByTestId} = render(<Barchart/>);
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        const barchartComponent = getByTestId("barchart");
+        expect(barchartComponent).toBeInTheDocument();
+    });
+
+    it('Accepts props', () => {
+        const items = [
+            { assignment: 1, fun: 3, difficulty: 2 },
+            { assignment: 2, fun: 5, difficulty: 1 },
+            { assignment: 3, fun: 2, difficulty: 4 },
+        ];
+
+        const { container } = render(<Barchart items={items} />);
+        // eslint-disable-next-line testing-library/no-node-access
+        expect(container.firstChild).toBeDefined();
+    });
 });
+
 
