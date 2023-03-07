8e6c8f5b23e6c3827ea142a798ed24d15b098cc8
Chaira Joel <chaira.cynthia.joel@gmail.com>
2023-03-07 23:03:20 +0100

    Added the contact page

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,8 +5,8 @@ function About(){
     return(
         <div>
             <main className="about" data-testid="about">
-                <h1>About</h1>
-                <p>Hello my name is Chaira Joel and this is my final assignment for Winc Academy</p>
+                <h2>About</h2>
+                <p>This is the final assignment for the front-end-development course of Winc Academy</p>
                 <p>For this final assignment, you will make a "real-life" project. This will be a project of which the end result will actually be used by Winc Academy: a Student Dashboard! Goal: to make it easy for Winc teachers to see how the students evaluate the assignments</p>
 
             </main>
