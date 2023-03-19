c7b29895bb6499f840b3be26ccfd1cddf4b431bb
Chaira Joel <chaira.cynthia.joel@gmail.com>
2023-03-19 13:56:49 +0100

    Extra

---
+++ src/UnitTests/Components/Home.test.js
@@ -0,0 +1,17 @@
+import React from "react"
+import {render, screen} from "@testing-library/react"
+import Home from "../../Components/Home";
+import {MemoryRouter} from "react-router-dom";
+
+describe('Homepage', () => {
+    it('Renders the Home component', ()=>{
+        render(
+            <MemoryRouter>
+                <Home/>
+            </MemoryRouter>  );
+        const homeComponent = screen.getByTestId("main-homepage");
+        expect(homeComponent).toBeInTheDocument();
+    });
+});
+
+
